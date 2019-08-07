32e70d98701620f47d8bd519b2bde07fcd040b6c
Vibhu <vibhugautam51@gmail.com>
2019-08-07 10:09:46 +0530

    Extra console logs removed

--- templates/simple.js
+++ templates/simple.js
@@ -16,9 +16,6 @@
 //     })
 //     })
 //     })
-
-    console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const firstpara = document.querySelector("#first")
